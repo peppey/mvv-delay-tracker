e8a6e5c11d47df206e8246ca8db676d4d7abaabd
Pia Baronetzky <pia@baronetzky.org>
2026-09-15 14:32:54 +0200

    Added stop.txt to permanent data

--- scripts/cloud_job.py
+++ scripts/cloud_job.py
@@ -38,6 +38,7 @@ STATIC_FILES = [
     "data/static/routes.txt",
     "data/static/trips.txt",
     "data/static/munich_stops.csv",
+    "data/static/stops.txt",
     "data/static/stop_times.txt",
     "data/static/calendar.txt",
     "data/static/calendar_dates.txt",
